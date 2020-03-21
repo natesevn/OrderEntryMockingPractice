694458996d31e99340c42b7f42968aa91d17799e
Nicholas Handaja <nihandaj@microsoft.com>
2020-03-20 17:57:54 -0700

    Initial setup to get tests working.

--- OrderEntryMockingPracticeTests/OrderServiceTests.cs
+++ OrderEntryMockingPracticeTests/OrderServiceTests.cs
@@ -1,6 +1,16 @@
-﻿namespace OrderEntryMockingPracticeTests
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace OrderEntryMockingPracticeTests
 {
+    [TestClass]
     public class OrderServiceTests
     {
+        [TestMethod]
+        public void TestMethodRunsProperly()
+        {
+            var testMsg = "hello";
+            Assert.AreEqual("hello", testMsg);
+        }
     }
 }
