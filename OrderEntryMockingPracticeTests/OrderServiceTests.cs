2bf22687a1f873965dace846edc7d74b25e2c2a8
Nicholas Handaja <nihandaj@microsoft.com>
2020-03-22 12:14:10 -0700

    Added test for duplicate Sku; Added misc files

--- OrderEntryMockingPracticeTests/OrderServiceTests.cs
+++ OrderEntryMockingPracticeTests/OrderServiceTests.cs
@@ -1,16 +1,80 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderEntryMockingPractice.Models;
 using System;
+using System.Linq;
 
 namespace OrderEntryMockingPracticeTests
 {
     [TestClass]
     public class OrderServiceTests
     {
-        [TestMethod]
-        public void TestMethodRunsProperly()
+        private Order order;
+
+        private OrderItem order1;
+        private OrderItem order2;
+        private OrderItem order3;
+
+        private Product product1;
+        private Product product2;
+        private Product product3;
+
+        [TestInitialize]
+        public void SetupProductsAndOrders()
         {
-            var testMsg = "hello";
-            Assert.AreEqual("hello", testMsg);
+            order = new Order();
+
+            product1 = new Product
+            {
+                Sku = "sku1"
+            };
+            order1 = new OrderItem
+            {
+                Product = product1
+            };
+
+            product2 = new Product
+            {
+                Sku = "sku2"
+            };
+            order2 = new OrderItem
+            {
+                Product = product2
+            };
+
+            product3 = new Product
+            {
+                Sku = "sku1"
+            };
+            order3 = new OrderItem
+            {
+                Product = product3
+            };
+        }
+
+        [TestMethod]
+        public void OrderItemsHaveUniqueSku()
+        {            
+            // Act
+            order.OrderItems.Add(order1);
+            order.OrderItems.Add(order2);
+
+            bool hasNoDuplicates = order.hasNoDuplicateSku();
+
+            // Assert
+            Assert.AreEqual(hasNoDuplicates, true, "Products with different SKUs considered same.");
+        }
+
+        [TestMethod]
+        public void OrderItemsHaveDuplicateSku()
+        {
+            // Act
+            order.OrderItems.Add(order1);
+            order.OrderItems.Add(order3);
+
+            bool hasNoDuplicates = order.hasNoDuplicateSku();
+
+            // Assert
+            Assert.AreEqual(hasNoDuplicates, false, "Products with same SKUs considered different.");
         }
     }
 }
