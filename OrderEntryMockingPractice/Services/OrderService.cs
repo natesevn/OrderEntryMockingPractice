2dc14e1863f0babd2c4bb0620c73bfdbd6464402
Nicholas Handaja <nihandaj@microsoft.com>
2020-03-23 14:50:24 -0700

    Added product in stock check to PlaceOrder

--- OrderEntryMockingPractice/Services/OrderService.cs
+++ OrderEntryMockingPractice/Services/OrderService.cs
@@ -1,10 +1,13 @@
 ﻿using OrderEntryMockingPractice.Models;
+using System;
 
 namespace OrderEntryMockingPractice.Services
 {
     public class OrderService
     {
         public const string DuplicateSkuError = "Duplicate SKUs in order.";
+        public const string NoStockError = "One or more products not in stock.";
+
         private IProductRepository _productRepo;
 
         public OrderService(IProductRepository productRepo)
@@ -17,9 +20,16 @@ namespace OrderEntryMockingPractice.Services
             // Validate order properties
             if (order.HasNoDuplicateSku() == false)
             {
-                throw new System.ArgumentException(DuplicateSkuError);
+                throw new ArgumentException(DuplicateSkuError);
             }
 
+            foreach(OrderItem items in order.OrderItems)
+            {
+                if(_productRepo.IsInStock(items.Product.Sku) == false)
+                {
+                    throw new ArgumentException(NoStockError);
+                }
+            }
 
             
 
