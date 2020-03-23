aafa6ebc05f2c6431db36efb41813cbb7a2379cc
Nicholas Handaja <nihandaj@microsoft.com>
2020-03-23 15:49:44 -0700

    Refactored tests and added some comments

--- OrderEntryMockingPracticeTests/OrderServiceTests.cs
+++ OrderEntryMockingPracticeTests/OrderServiceTests.cs
@@ -3,6 +3,7 @@ using Moq;
 using OrderEntryMockingPractice.Models;
 using OrderEntryMockingPractice.Services;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace OrderEntryMockingPracticeTests
@@ -10,83 +11,90 @@ namespace OrderEntryMockingPracticeTests
     [TestClass]
     public class OrderServiceTests
     {
-        private Order order;
-
-        private OrderItem order1;
-        private OrderItem order2;
-        private OrderItem order3;
-
-        private Product product1;
-        private Product product2;
-        private Product product3;
-
+        // SKU constants
+        private const string SkuUniqueOne = "sku1";
+        private const string SkuUniqueTwo = "sku2";
+        // Signifies product has duplicate SKU 
+        private const string SkuDuplicateOne = SkuUniqueOne;
+        // Signifies product is out of stock
+        private const string SkuOutOfStock = "outStock";
+        
+        // Mocked interfaces
         private Mock<IProductRepository> _mockedProductRepo;
-
+        
+        // Services
         private OrderService _orderService;
+
+        private Product CreateProduct(string name, string sku)
+        {
+            return new Product
+            {
+                Name = name,
+                Sku = sku
+            };
+        }
+
+        private OrderItem CreateOrderItem(Product product, int quantity = 0)
+        {
+            return new OrderItem
+            {
+                Product = product,
+                Quantity = quantity
+            };
+        }
+
+        private Order CreateOrder(
+            string skuOne, string skuTwo,
+            string productNameOne = "prodOne", string productNameTwo = "prodTwo")
+        {
+            Product product1 = CreateProduct(productNameOne, skuOne);
+            Product product2 = CreateProduct(productNameTwo, skuTwo);
+
+            OrderItem item1 = CreateOrderItem(product1);
+            OrderItem item2 = CreateOrderItem(product2);
+
+            Order order = new Order();
+            order.OrderItems = new List<OrderItem>
+            {
+                item1,
+                item2 
+            };
+
+            return order;        
+        }
 
         [TestInitialize]
         public void SetupProductsAndOrders()
         {
             // Setup Mocked Interfaces
             _mockedProductRepo = new Mock<IProductRepository>();
+            // Mocked product stock check returns true by default
+            _mockedProductRepo.Setup(x => x.IsInStock(It.IsAny<string>())).Returns(true);
+            // Mocked product stock check returns false if passed SkuOutOfStock
+            _mockedProductRepo.Setup(x => x.IsInStock(SkuOutOfStock)).Returns(false);
 
-            _orderService = new OrderService();
-
-            order = new Order();
-
-            product1 = new Product
-            {
-                Sku = "sku1"
-            };
-            order1 = new OrderItem
-            {
-                Product = product1
-            };
-
-            product2 = new Product
-            {
-                Sku = "sku2"
-            };
-            order2 = new OrderItem
-            {
-                Product = product2
-            };
-
-            product3 = new Product
-            {
-                Sku = "sku1"
-            };
-            order3 = new OrderItem
-            {
-                Product = product3
-            };
+            _orderService = new OrderService(_mockedProductRepo.Object);
         }
-
+       
         [TestMethod]
-        public void NoExceptionIfOrderItemsHaveUniqueSku()
-        {            
-            // Act
-            order.OrderItems.Add(order1);
-            order.OrderItems.Add(order2); 
-
+        public void NoExceptionIfOrderItemsHaveUniqueSkuAndInStock()
+        {
+            Order order = CreateOrder(SkuUniqueOne, SkuUniqueTwo);
+            
             try
             {
                 _orderService.PlaceOrder(order);
             }
             catch(ArgumentException ae)
             {
-                // Assert
-                Assert.Fail("Expected no exception.");
+                Assert.Fail("Expected no exception but received: " + ae.Message);
             }
         }
 
         [TestMethod]
         public void ExceptionIfOrderItemsHaveDuplicateSku()
         {
-            // Act
-            order.OrderItems.Add(order1);
-            order.OrderItems.Add(order2);
-            order.OrderItems.Add(order3);
+            Order order = CreateOrder(SkuUniqueOne, SkuDuplicateOne);
 
             try
             {
@@ -95,23 +103,25 @@ namespace OrderEntryMockingPracticeTests
             }
             catch (ArgumentException ae)
             {
-                // Assert
                 Assert.AreEqual(ae.Message, OrderService.DuplicateSkuError);
             }
         }
 
         [TestMethod]
-        public void OrderProductsInStock()
+        public void ExceptionIfProductsNotInStock()
         {
-            // Arrange
-            _mockedProductRepo.Setup(x => x.IsInStock(It.IsAny<string>())).Returns(true);
-            var test = _mockedProductRepo.Object;
+            Order order = CreateOrder(SkuOutOfStock, SkuUniqueOne);
 
-            // Act
-            var isInStock = test.IsInStock(order1.Product.Sku);
-
-            // Assert
-            Assert.AreEqual(isInStock, true);
+            try
+            {
+                _orderService.PlaceOrder(order);
+                Assert.Fail("The expected exception was not thrown.");
+            }
+            catch (ArgumentException ae)
+            {
+                Assert.AreEqual(ae.Message, OrderService.NoStockError);
+            }
         }
+
     }
 }
